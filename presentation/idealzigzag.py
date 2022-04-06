5f0849e6f0b786f82d7c274756fdd94f5f04fd08
Markus <markus.hartlage@fh-bielefeld.de>
2022-04-06 17:26:06 +0200

    fertig.

--- presentation/idealzigzag.py
+++ presentation/idealzigzag.py
@@ -3,10 +3,10 @@ import numpy as np
 f = 100e3
 
 if f == 100e3: # 100 khz
-	Ul, Uh, T, T0 = 0.0, 3.3, 1e-5, -9e-5
-	nump = 4
+	Ul, Uh, T, T0 = 0.0, 3.3, 1e-5, -95e-7
+	nump = 30
 elif f == 350e3: # 350khz
-	Ul, Uh, T, T0 = 0.0, 3.3, 3.4e-6, -10e-6
+	Ul, Uh, T, T0 = 0.0, 3.3, 3.4e-6, -1e-5
 	nump = 30
 
 p=np.array([[i*T/2 + T0, Ul * (i%2) + Uh * ((i+1)%2)] for i in range(nump)])
